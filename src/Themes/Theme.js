7ba72cacdac3208b080695e2f8f46be718c3d16b
Hiten Chowdhary <hiten.iitkgp@gmail.com>
2018-10-03 16:17:33 +0530

    Added some color options

--- src/Themes/Theme.js
+++ src/Themes/Theme.js
@@ -6,6 +6,18 @@ const palette = {
   secondary: {
     color: "#BF607B",
     contrastText: "#220020"
+  },
+  option1: {
+    color: "#051C3B",
+    contrastText: "#E5EFEE"
+  },
+  option2: {
+    color: "#332532",
+    contrastText: "#FF974F"
+  },
+  option3: {
+    color: "#EA2E49",
+    contrastText: "#F6F792"
   }
 };
 
